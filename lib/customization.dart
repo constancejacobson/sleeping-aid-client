68b9d6c50db1d19748cbda482a13c8049b9df39c
constancejacobson <constance.jacobson98@gmail.com>
2021-01-06 00:14:43 -0500

    init

---
+++ lib/customization.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<String> postCustomization(String age, String weight) async {
+  var params = {'age': age, 'weight': weight};
+  var uri = Uri.https('sleeping-aid-295720.web.app', '/customization', params);
+
+  final http.Response response = await http.put(uri);
+
+  if (response.statusCode == 200) {
+    return 'Customization updated';
+  }
+  return 'Customization failed';
+}
+
+class Customization extends StatefulWidget {
+  @override
+  _CustomizationState createState() => _CustomizationState();
+}
+
+class _CustomizationState extends State<Customization> {
+  final _ageController = TextEditingController();
+  final _weightController = TextEditingController();
+  Future<String> _futureCustomization;
+
+  void dispose() {
+    _ageController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return (_futureCustomization == null)
+        ? Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                  controller: _ageController,
+                  decoration: InputDecoration(
+                      labelText: 'Enter baby\'s age in months')),
+              TextFormField(
+                  controller: _weightController,
+                  decoration: InputDecoration(
+                      labelText: 'Enter baby\'s weight in pounds')),
+              ElevatedButton(
+                  onPressed: () => {
+                        setState(() => {
+                              _futureCustomization = postCustomization(
+                                  _ageController.text, _weightController.text)
+                            })
+                      },
+                  child: Text('Save'))
+            ],
+          )
+        : FutureBuilder<String>(
+            future: _futureCustomization,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              return CircularProgressIndicator();
+            },
+          );
+  }
+}
