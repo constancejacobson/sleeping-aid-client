08737e9d96a45ee7d133013975f7cc2a2df74561
constancejacobson <constance.jacobson98@gmail.com>
2021-01-08 17:05:46 -0500

    makeover

--- lib/customization.dart
+++ lib/customization.dart
@@ -29,6 +29,13 @@ class _CustomizationState extends State<Customization> {
     super.dispose();
   }
 
+  void onSave() {
+    setState(() => {
+          _futureCustomization =
+              postCustomization(_ageController.text, _weightController.text)
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return (_futureCustomization == null)
@@ -39,31 +46,51 @@ class _CustomizationState extends State<Customization> {
                   controller: _ageController,
                   decoration: InputDecoration(
                       labelText: 'Enter baby\'s age in months')),
+              SizedBox(height: 20),
               TextFormField(
                   controller: _weightController,
                   decoration: InputDecoration(
                       labelText: 'Enter baby\'s weight in pounds')),
-              ElevatedButton(
-                  onPressed: () => {
-                        setState(() => {
-                              _futureCustomization = postCustomization(
-                                  _ageController.text, _weightController.text)
-                            })
-                      },
-                  child: Text('Save'))
+              SizedBox(height: 20),
+              ElevatedButton(onPressed: () => {onSave()}, child: Text('Save'))
             ],
           )
-        : FutureBuilder<String>(
-            future: _futureCustomization,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
+        : DefaultTextStyle(
+            style: Theme.of(context).textTheme.headline3,
+            textAlign: TextAlign.center,
+            child: FutureBuilder<String>(
+              future: _futureCustomization,
+              builder: (context, snapshot) {
+                List<Widget> children;
+                if (snapshot.hasData) {
+                  children = <Widget>[
+                    Icon(Icons.check_circle_outline,
+                        color: Colors.green, size: 60),
+                    Text(snapshot.data),
+                  ];
+                } else if (snapshot.hasError) {
+                  children = <Widget>[
+                    Icon(Icons.error_outline, color: Colors.red, size: 60),
+                    Text('Error:  ${snapshot.error}'),
+                  ];
+                } else {
+                  children = <Widget>[
+                    SizedBox(
+                      child: CircularProgressIndicator(),
+                      width: 60,
+                      height: 60,
+                    ),
+                  ];
+                }
 
-              return CircularProgressIndicator();
-            },
-          );
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: children,
+                  ),
+                );
+              },
+            ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -61,12 +61,12 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Customization(),
-    );
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body:
+            Padding(padding: const EdgeInsets.all(8), child: Customization()));
   }
 }
